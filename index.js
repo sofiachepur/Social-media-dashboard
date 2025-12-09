d60e338b9579bdd97dddb81c9d62caed5fc0aabf
sofiachepur <chepursofia27@ gmail.com>
2025-12-09 23:23:00 +0200

    додано дату оновлення в футер

--- index.js
+++ index.js
@@ -3,3 +3,16 @@
     toggle.addEventListener("change",()=>{
         document.body.classList.toggle("dark");
 });
+
+    const updateEl = document.getElementById('last-update');
+    const now = new Date();
+
+
+    const formattedDate = now.getFullYear() + '-' +
+        String(now.getMonth() + 1).padStart(2, '0') + '-' +
+        String(now.getDate()).padStart(2, '0') + ' ' +
+        String(now.getHours()).padStart(2, '0') + ':' +
+        String(now.getMinutes()).padStart(2, '0') + ':' +
+        String(now.getSeconds()).padStart(2, '0');
+
+    updateEl.textContent = formattedDate;
