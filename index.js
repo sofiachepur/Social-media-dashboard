a18f687d6ccfca6d0917da69aa65408c2fa732ec
sofiachepur <chepursofia27@ gmail.com>
2025-12-10 20:15:32 +0200

    desktop version

--- index.js
+++ index.js
@@ -16,3 +16,34 @@
         String(now.getSeconds()).padStart(2, '0');
 
     updateEl.textContent = formattedDate;
+
+
+
+
+
+
+
+    function createSnowflake() {
+        const snowflake = document.createElement("div");
+        snowflake.classList.add("snowflake");
+        snowflake.textContent = "❄";
+
+
+        const size = Math.random() * 20 + 10;
+        const left = Math.random() * window.innerWidth;
+        const duration = Math.random() * 5 + 5;
+        const opacity = Math.random() * 0.5 + 0.5;
+
+        snowflake.style.left = left + "px";
+        snowflake.style.fontSize = size + "px";
+        snowflake.style.opacity = opacity;
+        snowflake.style.animationDuration = duration + "s";
+
+        document.body.appendChild(snowflake);
+
+
+        setTimeout(() => snowflake.remove(), duration * 1000);
+    }
+
+
+    setInterval(createSnowflake, 200);
